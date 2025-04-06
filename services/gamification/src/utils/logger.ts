3001cbd618dba5bb1937e05bc94680aab9137e54
Vid Kotnik <kotnik.vid@gmail.com>
2025-04-06 17:03:19 +0200

    Update gitignore

--- services/gamification/src/utils/logger.ts
+++ services/gamification/src/utils/logger.ts
@@ -10,7 +10,7 @@ const logger = winston.createLogger({
   ),
   transports: [
     new winston.transports.Console(),
-    new winston.transports.File({ filename: "logs/streakService.log" }),
+    new winston.transports.File({ filename: "logs/gamificationLogs.log" }),
   ],
 });
 
